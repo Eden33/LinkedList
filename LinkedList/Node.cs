7bdcba386c79e2d957f46835b2171d9e7ecf5dd1
Eden33 <eduard.gopp@gmail.com>
2014-04-16 14:13:03 +0200

    initial commit

---
+++ LinkedList/Node.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Node
+    {
+        private object value = null;
+        private Node next = null;
+
+        public Node()
+        {
+        }
+
+        public Node(object o)
+        {
+            value = o;
+        }
+
+        public void InsertAtEnd(object o)
+        {
+            if (value == null)
+                value = o;
+            else
+            {
+                if (next == null)
+                {
+                    next = new Node(o);
+                }
+                else
+                    next.InsertAtEnd(o);
+            }
+        }
+
+        public bool Exists(object o)
+        {
+            if (value != null)
+            {
+                if (value.Equals(o))
+                    return true;
+                else
+                {
+                    if (next != null)
+                        return next.Exists(o);
+                    else return false;
+                }
+            }
+            else return false;
+        }
+
+        public String NodeToString()
+        {
+            if (value == null)
+                return "empty";
+            else
+            {
+                if (next == null)
+                    return value.ToString() + " - empty";
+                else return value.ToString() + " - " + next.NodeToString();
+            }
+        }
+
+        public void ChangeAtPosition(int index, object onew)
+        {
+            if(index == 0)
+            {
+                value = onew;
+            }
+            else
+            {
+                if (next != null)
+                    next.ChangeAtPosition(--index, onew);
+            }
+        }
+
+        public void insertAt(int index, object o)
+        {
+            if (index == 0)
+            {
+                if (value == null)
+                    value = 0;
+                else
+                {
+                    if (next != null)
+                    {
+                        Node new_node = new Node(value);
+                        value = o;
+                        new_node.next = next;
+                        next = new_node;
+                    }
+                    else
+                    {
+                        next = new Node(value);
+                        value = o;
+                    }
+                }
+            }
+            else
+            {
+                if (next != null)
+                    next.insertAt(--index, o);
+            }
+        }
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Node n = new Node();
+            n.InsertAtEnd(5);
+            n.InsertAtEnd(10);
+            n.InsertAtEnd(15);
+            Console.WriteLine(n.NodeToString());
+            n.ChangeAtPosition(1, 11);
+            Console.WriteLine(n.NodeToString());
+            n.insertAt(0, 10);
+            Console.WriteLine(n.NodeToString());
+            
+            Console.ReadKey();
+        }
+    }
+}
